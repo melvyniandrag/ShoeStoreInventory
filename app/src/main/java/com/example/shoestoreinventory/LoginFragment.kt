eb1bf8137e080be975ade0d4bd5b81a46e839fe3
melvyn drag <melvyniandrag@gmail.com>
2023-05-01 16:33:11 -0400

    refactored to use fragments

---
+++ app/src/main/java/com/example/shoestoreinventory/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.shoestoreinventory
+
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.shoestoreinventory.databinding.FragmentLoginBinding
+
+class LoginFragment() : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+            inflater, R.layout.fragment_login, container, false)
+
+        // layout crowded on login screen in landscape mode.
+        // TODO create an alternate landscape layout?
+        // for now, just hide the supportActionBar so theres a little more space.
+        (activity as AppCompatActivity).supportActionBar?.hide()
+
+        return binding.root
+    }
+
+
+}
